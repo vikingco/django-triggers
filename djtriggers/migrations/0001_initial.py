4ce67a18daf38c778fc1a1fcac572796b72cb5d2
Pieter De Decker <pieterdd@users.noreply.github.com>
2017-12-11 11:44:11 +0100

    VD-783: Prepare django-triggers for Django 2.0 (#24)
    * Rewrite URL resolving imports
    
    * Explicitly specify on_delete
    
    This used to be the default value, but it is required as of Django 2.0.
    
    * Mark as compatible with the Django 2.0.x family
    
    * Correct install order
    
    * Don't build with Django 2.0.x on Python 2.7
    
    * Bump version

--- djtriggers/migrations/0001_initial.py
+++ djtriggers/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('result', models.TextField()),
-                ('trigger', models.ForeignKey(to='djtriggers.Trigger')),
+                ('trigger', models.ForeignKey(to='djtriggers.Trigger', on_delete=models.CASCADE)),
             ],
             options={
             },
